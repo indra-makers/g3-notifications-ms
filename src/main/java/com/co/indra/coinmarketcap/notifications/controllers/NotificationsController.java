ef90697df51270a061dbe0e3107bf438e418081e
Juan Carlos <jhcarlos181@protonmail.com>
2022-05-16 19:06:42 -0500

    Se eliminan todos los metodos de modulo notificaciones

--- src/main/java/com/co/indra/coinmarketcap/notifications/controllers/NotificationsController.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/controllers/NotificationsController.java
@@ -1,6 +1,5 @@
 package com.co.indra.coinmarketcap.notifications.controllers;
 
-
 import com.co.indra.coinmarketcap.notifications.model.entities.Notifications;
 import com.co.indra.coinmarketcap.notifications.model.entities.UserNotificationsData;
 import com.co.indra.coinmarketcap.notifications.model.requests.NotificationsDataRequest;
@@ -15,68 +14,61 @@ import java.util.List;
 @RequestMapping("/notifications")
 public class NotificationsController {
 
-    @Autowired
-    private NotificationsService notificationsService;
+	@Autowired
+	private NotificationsService notificationsService;
 
-    @Autowired
-    private UserNotificationsDataService userNotificationsDataService;
+	@Autowired
+	private UserNotificationsDataService userNotificationsDataService;
 
-    /********Comienzo de controladores para entidad Notifications*/
+	/******** Comienzo de controladores para entidad Notifications */
 
-    //Crear nueva notificacion
-    @PostMapping("/sendnotification")
-    public void create(@RequestBody Notifications notifications){
-        notificationsService.registerNotification(notifications);
+	// Crear nueva notificacion
+	@PostMapping("/sendnotification")
+	public void create(@RequestBody Notifications notifications) {
 
-    }
+	}
 
-    //Obtener notificaciones por tipo de notificacion
-    @GetMapping
-    public List<Notifications> getNotificationsByType(@RequestParam(name = "searchnotification") String type){
-        return notificationsService.getNotificationsByType(type);
-    }
+	// Obtener notificaciones por tipo de notificacion
+	@GetMapping
+	public List<Notifications> getNotificationsByType(@RequestParam(name = "searchnotification") String type) {
+		return null;
+	}
 
-    //Obtener notificaciones por el id de la notificacion
-    @GetMapping("/searchnotificationID")
-    public List<Notifications> getNotificationsByIdNotifications(@RequestParam(name = "id") Long id_notifications){
-        return notificationsService.getNotificationsByIdIdNotifications(id_notifications);
+	// Obtener notificaciones por el id de la notificacion
+	@GetMapping("/searchnotificationID")
+	public List<Notifications> getNotificationsByIdNotifications(@RequestParam(name = "id") Long id_notifications) {
+		return null;
+	}
 
-    }
+	// Eliminar notificacion por medio del id
+	@DeleteMapping
+	public void remove(@RequestParam(name = "deletenotification") Long id_notifications) {
 
-    //Eliminar notificacion por medio del id
-    @DeleteMapping
-    public void remove(@RequestParam(name = "deletenotification") Long id_notifications){
-        notificationsService.removeNotification(id_notifications);
-    }
+	}
 
+	/****************
+	 * Comienzo de controladores para entidad UserNotificationData
+	 **/
 
-    /****************Comienzo de controladores para entidad UserNotificationData**/
+	// Agregar a una notificacion sus datos de envio
+	@PostMapping("/{id_notifications}/usernotificationsdata")
+	public void createDataToNotification(@RequestBody NotificationsDataRequest request,
+			@PathVariable("id_notifications") Long id_notifications) {
 
-    //Agregar a una notificacion sus datos de envio
-    @PostMapping("/{id_notifications}/usernotificationsdata")
-    public void createDataToNotification(@RequestBody NotificationsDataRequest request,
-                                         @PathVariable("id_notifications") Long id_notifications){
+	}
 
-        userNotificationsDataService.registerUserNotificationData(request.getMail(), request.getPhone_number(), id_notifications);
+	// Obtener los datos de envio segun el id de la notificacion suministrada por la
+	// URL
+	@GetMapping("/{id_notifications}/usernotificationsdata")
+	public List<UserNotificationsData> getNotificationDataByIdNotification(
+			@PathVariable("id_notifications") Long id_notifications) {
+		return null;
+	}
 
-    }
+	// Eliminar los datos de envio por medio del id Notifications FK
+	@DeleteMapping("/{id_notifications}/usernotificationsdata")
+	public void removeUserDataByIdNotifications(@PathVariable("id_notifications") Long id_notifications) {
 
-
-
-    //Obtener los datos de envio segun el id de la notificacion suministrada por la URL
-    @GetMapping ("/{id_notifications}/usernotificationsdata")
-    public List<UserNotificationsData> getNotificationDataByIdNotification(
-                                         @PathVariable("id_notifications") Long id_notifications){
-
-        return userNotificationsDataService.getNotificationDataByIdNotification(id_notifications);
-
-    }
-
-    //Eliminar los datos de envio por medio del id Notifications FK
-    @DeleteMapping ("/{id_notifications}/usernotificationsdata")
-    public void removeUserDataByIdNotifications(@PathVariable("id_notifications") Long id_notifications){
-        userNotificationsDataService.removeUserNotificationData(id_notifications);
-    }
-
+	}
 
 }
--- src/main/java/com/co/indra/coinmarketcap/notifications/model/entities/Notifications.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/model/entities/Notifications.java
@@ -1,73 +1,79 @@
 package com.co.indra.coinmarketcap.notifications.model.entities;
 
-
-
-
 import java.io.Serializable;
 import java.util.Date;
 
 public class Notifications implements Serializable {
 
+	private Long id_notifications;
 
-    private Long id_notifications;
+	private String type;
 
-    private String type;
+	private String message;
 
-    private String message;
+	private Date sent_at;
 
-    private Date sent_at;
+	private Long id_user;
 
-    private Long id_user;
+	private String sent_to;
 
+	public Notifications() {
+	}
 
-    public Notifications() {
-    }
+	// Constructor with attributes
+	public Notifications(String type, String message, Long id_user, String sent_to) {
+		this.type = type;
+		this.message = message;
+		this.id_user = id_user;
+		this.sent_to = sent_to;
+	}
 
-    //Constructor with attributes
-    public Notifications(String type, String message, Long id_user) {
-        this.type = type;
-        this.message = message;
-        this.id_user = id_user;
-    }
+	// Getters and Setters
+	public Long getId_notifications() {
+		return id_notifications;
+	}
 
-    //Getters and Setters
-    public Long getId_notifications() {
-        return id_notifications;
-    }
+	public void setId_notifications(Long id_notifications) {
+		this.id_notifications = id_notifications;
+	}
 
-    public void setId_notifications(Long id_notifications) {
-        this.id_notifications = id_notifications;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public Date getSent_at() {
+		return sent_at;
+	}
 
-    public Date getSent_at() {
-        return sent_at;
-    }
+	public void setSent_at(Date sent_at) {
+		this.sent_at = sent_at;
+	}
 
-    public void setSent_at(Date sent_at) {
-        this.sent_at = sent_at;
-    }
+	public Long getId_user() {
+		return id_user;
+	}
 
-    public Long getId_user() {
-        return id_user;
-    }
+	public void setId_user(Long id_user) {
+		this.id_user = id_user;
+	}
 
-    public void setId_user(Long id_user) {
-        this.id_user = id_user;
-    }
+	public String getSent_to() {
+		return sent_to;
+	}
+
+	public void setSent_to(String sent_to) {
+		this.sent_to = sent_to;
+	}
 }
